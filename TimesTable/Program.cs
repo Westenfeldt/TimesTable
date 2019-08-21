cee6f851a357d22278f0b249225f1e73bbe8cf99
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-21 11:44:12 +0200

    fjernet udnødvendigt kode

--- TimesTable/Program.cs
+++ TimesTable/Program.cs
@@ -50,9 +50,9 @@ namespace TimesTable
             switch (tabel)
             {
                 case "1": // Et-Tabel
-                    for (i = 1; i >= 1 && i <= 10; i += 1)
+                    for (i = 1; i >= 1 && i <= 10; i ++ )
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i et-tabellen er: {0}", sum1);
@@ -61,7 +61,7 @@ namespace TimesTable
                 case "2": // To-Tabel
                     for (i = 2; i >= 2 && i <= 20; i += 2)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i to-tabellen er: {0}", sum2);
@@ -69,7 +69,7 @@ namespace TimesTable
                 case "3": // Tre-Tabel
                     for (i = 3; i >= 3 && i <= 30; i += 3)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i tre-tabellen er: {0}", sum3);
@@ -77,7 +77,7 @@ namespace TimesTable
                 case "4": // Fire-Tabel
                     for (i = 4; i >= 4 && i <= 40; i += 4)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i fire-tabellen er: {0}", sum4);
@@ -85,7 +85,7 @@ namespace TimesTable
                 case "5": // Fem-Tabel
                     for (i = 5; i >= 5 && i <= 50; i += 5)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i fem-tabellen er: {0}", sum5);
@@ -93,7 +93,7 @@ namespace TimesTable
                 case "6": // Seks-Tabel
                     for (i = 6; i >= 6 && i <= 60; i += 6)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i seks-tabellen er: {0}", sum6);
@@ -101,7 +101,7 @@ namespace TimesTable
                 case "7": // Syv-Tabel
                     for (i = 7; i >= 7 && i <= 70; i += 7)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i syv-tabellen er: {0}", sum7);
@@ -109,7 +109,7 @@ namespace TimesTable
                 case "8": // Otte-Tabel
                     for (i = 8; i >= 8 && i <= 80; i += 8)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i otte-tabellen er: {0}", sum8);
@@ -117,7 +117,7 @@ namespace TimesTable
                 case "9": // Ni-Tabel
                     for (i = 9; i >= 9 && i <= 90; i += 9)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i ni-tabellen er: {0}", sum9);
@@ -125,7 +125,7 @@ namespace TimesTable
                 case "10": // Ti-Tabel
                     for (i = 10; i >= 10 && i <= 100; i += 10)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.WriteLine(i);
 
                     }
                     Console.WriteLine("Summen af alle tal i ti-tabellen er: {0}", sum10);
